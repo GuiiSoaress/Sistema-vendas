2e039fb65456cd341e52d81f98c40ae307e012e0
GUILHERME SOARES DA SILVA <guilherme.silva957@senaisp.edu.br>
2026-09-16 19:40:40 -0300

    FEAT: Adicionado classe ClienteService.java

---
+++ src/main/java/br/com/aweb/sistema_vendas/service/ClienteService.java
@@ -0,0 +1,71 @@
+package br.com.aweb.sistema_vendas.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import br.com.aweb.sistema_vendas.model.Cliente;
+import br.com.aweb.sistema_vendas.model.Produto;
+import br.com.aweb.sistema_vendas.repository.ClienteRepository;
+import jakarta.transaction.Transactional;
+
+public class ClienteService {
+
+    private final ClienteRepository clienteRepository;
+
+    public ClienteService(ClienteRepository clienteRepository) {
+        this.clienteRepository = clienteRepository;
+    }
+
+    // Create
+    @Transactional
+    public Cliente salvar(Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    // Read
+    public List<Cliente> listarTodos() {
+        return clienteRepository.findAll();
+    }
+
+    public Optional<Cliente> buscarPorId(Long id) {
+        return clienteRepository.findById(id);
+    }
+
+    // Update
+    @Transactional
+    public Cliente atualizar(Long id, Cliente clienteAtualizado) {
+        var optionalCliente = buscarPorId(id);
+        if (!optionalCliente.isPresent())
+            throw new IllegalArgumentException("Cliente não encontrado.");
+
+        var clienteExistente = optionalCliente.get();
+
+        clienteExistente.setNome(clienteAtualizado.getNome());
+        clienteExistente.setEmail(clienteAtualizado.getEmail());
+        clienteExistente.setCpf(clienteAtualizado.getCpf());
+        clienteExistente.setTelefone(clienteAtualizado.getTelefone());
+
+        clienteExistente.setLogradouro(clienteAtualizado.getLogradouro());
+        clienteExistente.setNumero(clienteAtualizado.getNumero());
+        clienteExistente.setComplemento(clienteAtualizado.getComplemento());
+        clienteExistente.setBairro(clienteAtualizado.getBairro());
+        clienteExistente.setCidade(clienteAtualizado.getCidade());
+        clienteExistente.setUf(clienteAtualizado.getUf());
+        clienteExistente.setCep(clienteAtualizado.getCep());
+
+        var clienteSalvo = clienteRepository.save(clienteExistente);
+        return clienteSalvo;
+    }
+
+    // Delete
+    @Transactional
+    public void excluir(Long id) {
+        var optionopalCliente = buscarPorId(id);
+        if (!optionopalCliente.isPresent())
+            throw new IllegalArgumentException("Cliente não encontrado.");
+
+        clienteRepository.deleteById(id);
+    }
+
+
+}
